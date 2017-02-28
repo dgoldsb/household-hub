589ed9aeb2b59f485a169d4e47cdf37b9eb094b5
dgoldsb <dgoldsb@live.nl>
2017-02-28 17:58:07 +0100

    Can send gmail reminders now.

--- scripts/updatedb.py
+++ scripts/updatedb.py
@@ -7,6 +7,13 @@ import subprocess
 import sqlite3
 import time
 import os
+import csv
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+import smtplib
+import sys
 
 ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
 
@@ -34,7 +41,7 @@ def updatedb():
         dates.append(curs.fetchone()[0])
 
     curs.execute("SELECT date_todo, CID FROM chorelog WHERE date_todo"\
-                 " > DATE('now','weekday 0','-6 day')")
+                 " > DATE('now','weekday 0','-8 day')")
     pairs_done = curs.fetchall()
 
     curs.execute('SELECT DISTINCT CID FROM chores')
@@ -83,10 +90,41 @@ def sendalert(parent, address, recipient):
     '''
     Sends alerts to someone with a chore coming up the next day
     '''
-    message = "Yo "+recipient+", vergeet niet dat je dit moet gaan doen (zie titel)"
-    debug = subprocess.check_output(['mail', '-s', parent, address, '<<<', message])
+    body = "Yo "+recipient+", vergeet niet dat je dit moet gaan doen (zie titel)"
 
-    return debug
+    address = None
+    pwd = None
+    with open(os.path.join(ROOT, 'scripts/email.csv'), 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            address = row[0]
+            pwd = row[1]
+    send_mail(address, [address], parent, body, None, pwd)
+    return 0
+
+def send_mail(send_from, recipients, subject, text, filename, pwd):
+    '''
+    Sends email, recipients is a list
+    '''
+    recipients = recipients
+    emaillist = [elem.strip().split(',') for elem in recipients]
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = send_from
+    msg['Reply-to'] = send_from
+    msg.preamble = text
+    if filename is not None:
+        part = MIMEText("\nPlease find the attached file")
+        msg.attach(part)
+        part = MIMEApplication(open(filename, "rb").read())
+        part.add_header('Content-Disposition', 'attachment', filename=filename)
+        msg.attach(part)
+    server = smtplib.SMTP("smtp.gmail.com:587")
+    server.ehlo()
+    server.starttls()
+    server.login(send_from, pwd)
+    server.sendmail(msg['From'], emaillist, msg.as_string())
+
 
 def findalerts():
     '''
@@ -96,9 +134,9 @@ def findalerts():
     conn.text_factory = str
     curs = conn.cursor()
 
-    # Send reminders for chores
-    job = "SELECT c.chore, b.first_name, b.email FROM (SELECT * FROM chorelog "\
-          "WHERE date_todo == DATE('now', '+1 day') AND finished = 0) as a"\
+    # Send reminders for chores that are today
+    job = "SELECT c.name, b.first_name, b.email FROM (SELECT * FROM chorelog "\
+          "WHERE date_todo == DATE('now') AND finished = 0) as a "\
           "LEFT JOIN (SELECT * FROM housemates) as b ON a.UID = b.UID "\
           "LEFT JOIN (SELECT * FROM chores) as c ON a.CID = c.CID"
     curs.execute(job)
@@ -106,6 +144,19 @@ def findalerts():
     for reminder in reminders:
         print('Sending reminder to %s (%s) about %s...' % reminder)
         sendalert(reminder[0], reminder[2], reminder[1])
+
+    # Send reminders for chores that are almost overdue
+    job = "SELECT c.name, b.first_name, b.email FROM (SELECT * FROM chorelog "\
+          "WHERE date_todo == DATE('now', '-1 day') AND finished = 0) as a "\
+          "LEFT JOIN (SELECT * FROM housemates) as b ON a.UID = b.UID "\
+          "LEFT JOIN (SELECT * FROM chores) as c ON a.CID = c.CID"
+
+    curs.execute(job)
+    reminders = curs.fetchall()
+    for reminder in reminders:
+        if reminder[2] is not None:
+            print('Sending reminder to %s (%s) about %s...' % reminder)
+            sendalert(reminder[0], reminder[2], reminder[1])
 
     # TODO: Send reminders for reminders
 
@@ -120,9 +171,21 @@ def main():
         print(bufile)
         debug = subprocess.check_output(['cp', '-f', os.path.join(ROOT, 'database/hub.db')
                                          , bufile])
+
+        address = None
+        pwd = None
+        admin = None
+        with open(os.path.join(ROOT, 'scripts/email.csv'), 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                address = row[0]
+                pwd = row[1]
+                admin = row[2]
+                break
+        send_mail(address, [admin], 'Backup '+bufile, 'See attachment.', bufile, pwd)
         print(debug)
     updatedb()
-    #findalerts()
+    findalerts()
 
 if __name__ == "__main__":
     main()
