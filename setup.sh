b8730313a20d213b2f2d8c9647caf7e525c83b78
Dylan <dgoldsb@live.nl>
2017-03-01 13:22:58 +0100

    Added the cronjob to the setup procedure.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,27 @@
+DIR = "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 mkdir -p database
-sqlite3 database/hub.db < ./scripts/setupDB.sql
-sqlite3 database/hub.db < ./scripts/initializeDB.sql
-python ./scripts/updatedb.py
+
+# Build and fill the database if it does not exist
+if [ ! -f ./database/hub.db ]; then
+    sqlite3 database/hub.db < DIR/scripts/setupDB.sql
+    sqlite3 database/hub.db < DIR/scripts/initializeDB.sql
+    python DIR/scripts/updatedb.py
+fi
+
+# If cronjob does not exist for update (weekly), create
+grep $USER+' python '+DIR+'/scripts/updateDB.py' /etc/crontab || echo '0  12  *  *  1 '+$USER+' python '+DIR+'/scripts/updateDB.py' >> /etc/crontab
+
+# If cronjob does not exist for reminders (daily), create
+grep $USER+' python '+DIR+'/scripts/sendreminders.py' /etc/crontab || echo '0  12  *  *  * '+$USER+' python '+DIR+'/scripts/sendreminders.py' >> /etc/crontab
+
+# If cronjob does not exist for launching the flask page (startup), create
+grep $USER+' python '+DIR+'/app/hub/hub.py &' /etc/crontab || echo '@REBOOT '+$USER+' python '+DIR+'/app/hub/hub.py &' >> /etc/crontab
+
+# Reboot the machine for crontab to take effect
+echo "Reboot the machine for everything to take effect."
+read -p  "Do you want to reboot (y/n)? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo reboot
+fi
