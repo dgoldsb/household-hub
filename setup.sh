086d420f9841454280fb2011e61ab85aeeee9d6b
dgoldsb <dgoldsb@live.nl>
2017-03-12 14:48:30 +0000

    Setup not working

--- setup.sh
+++ setup.sh
@@ -3,20 +3,20 @@ DIR = "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p database
 
 # Build and fill the database if it does not exist
-if [ ! -f ./database/hub.db ]; then
-    sqlite3 database/hub.db < DIR/scripts/setupDB.sql
-    sqlite3 database/hub.db < DIR/scripts/initializeDB.sql
-    python DIR/scripts/updatedb.py
+if [ ! -f $DIR/database/hub.db ]; then
+    sqlite3 $DIR/database/hub.db < $DIR/scripts/setupDB.sql
+    sqlite3 $DIR/database/hub.db < $DIR/scripts/initializeDB.sql
+    python $DIR/DIR/scripts/updatedb.py
 fi
 
 # If cronjob does not exist for update (weekly), create
-grep $USER+' python '+DIR+'/scripts/updateDB.py' /etc/crontab || echo '0  12  *  *  1 '+$USER+' python '+DIR+'/scripts/updateDB.py' >> /etc/crontab
+grep $USER+' python '+$DIR+'/scripts/updateDB.py' /etc/crontab || echo '0  12  *  *  1 '+$USER+' python '+$DIR+'/scripts/updateDB.py' >> /etc/crontab
 
 # If cronjob does not exist for reminders (daily), create
-grep $USER+' python '+DIR+'/scripts/sendreminders.py' /etc/crontab || echo '0  12  *  *  * '+$USER+' python '+DIR+'/scripts/sendreminders.py' >> /etc/crontab
+grep $USER+' python '+$DIR+'/scripts/sendreminders.py' /etc/crontab || echo '0  12  *  *  * '+$USER+' python '+$DIR+'/scripts/sendreminders.py' >> /etc/crontab
 
 # If cronjob does not exist for launching the flask page (startup), create
-grep $USER+' python '+DIR+'/app/hub/hub.py &' /etc/crontab || echo '@REBOOT '+$USER+' python '+DIR+'/app/hub/hub.py &' >> /etc/crontab
+grep $USER+' python '+$DIR+'/app/hub/hub.py &' /etc/crontab || echo '@REBOOT '+$USER+' python '+$DIR+'/app/hub/hub.py &' >> /etc/crontab
 
 # Reboot the machine for crontab to take effect
 echo "Reboot the machine for everything to take effect."
